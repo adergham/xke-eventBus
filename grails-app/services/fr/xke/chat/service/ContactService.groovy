07726dcf6b581f45574a940ea9b658003e9df990
Alexandre Dergham <adergham@xebia.fr>
2013-01-01 17:26:18 +0100

    modification de ContactService : l'uri est desormais défini en conf et le nom de l'utilisateur courant en envoyé dans la notification

--- grails-app/services/fr/xke/chat/service/ContactService.groovy
+++ grails-app/services/fr/xke/chat/service/ContactService.groovy
@@ -16,13 +16,14 @@ class ContactService {
             def query = [:]
             query["myIp"] = grailsApplication.config.chat.my.ip
             query["myPort"] = grailsApplication.config.chat.my.port
+            query["myName"] = grailsApplication.config.chat.username
             query["targetIp"] = contact.ip
             query["targetPort"] = contact.port
 
             def uri = "http://${query["targetIp"]}:${query["targetPort"]}"
-
+            def path = grailsApplication.config.chat.contact.notify.path
             withHttp(uri: uri){
-                post(path:"/contact/notify", query:query)
+                post(path:path, query:query)
             }
         }
     }

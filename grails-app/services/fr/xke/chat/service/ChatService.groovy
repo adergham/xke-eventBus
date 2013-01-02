f2586ebd2bfc4dd41f94cef14443b6c52d1b0194
Alexandre Dergham <adergham@xebia.fr>
2013-01-02 14:38:37 +0100

    ChatService : implémentation de broadcastMessage

--- grails-app/services/fr/xke/chat/service/ChatService.groovy
+++ grails-app/services/fr/xke/chat/service/ChatService.groovy
@@ -1,9 +1,13 @@
 package fr.xke.chat.service
 
+import fr.xke.chat.domain.Contact
 import fr.xke.chat.domain.Message
 import grails.events.Listener
+import groovyx.gpars.ParallelEnhancer
 
 class ChatService {
+
+    def grailsApplication
 
     def logMessage(author, text) {
 
@@ -18,9 +22,26 @@ class ChatService {
     @Listener(namespace='browser', topic="messageInput")
     def broadcastMessage(inputMessage){
         // we retrieve the list of contacts
+        def contacts = Contact.list()
 
         // parallelify the list before broadcasting
+        ParallelEnhancer.enhanceInstance(contacts)
 
         // broadcast the message
+        def authorName = grailsApplication.config.chat.username
+        def path = grailsApplication.config.chat.message.addMessage.path
+        def query = [author: authorName, message: inputMessage]
+
+        contacts.eachParallel {
+            def uri = "http://${it.ip}:${it.port}"
+
+            withHttp(uri: uri){
+                post(path:path, query:query)
+            }
+        }
+
+        // add the message into log
+        logMessage(authorName, inputMessage)
     }
+
 }

a6627a0f10c44c116f4c8460fd876c9d6a74a2b2
Alexandre Dergham <adergham@xebia.fr>
2013-01-05 22:43:39 +0100

    amelioration de test d'integration

--- test/integration/fr/xke/chat/service/ContactServiceIntegrationTests.groovy
+++ test/integration/fr/xke/chat/service/ContactServiceIntegrationTests.groovy
@@ -7,17 +7,18 @@ class ContactServiceIntegrationTests extends GroovyTestCase {
 
     def contactService
 
-    void testAfterInsertContactListener(){
+    void testAfterInsertValidContactListener(){
 
-        def hasCalledWS = false
-        contactService.metaClass.withHttp = {Map args, Closure closure ->
-            hasCalledWS=true
-            Exception.metaClass.toString = {->""}
-        return}
+        def hasFiredDisplayEvent = false
+        contactService.on("addContactToList"){
+            hasFiredDisplayEvent = true
 
-        def contact  = new Contact(ip: "127.0.0.1", port: "8080", name : "jack", isValid: false)
+        }
+
+        def contact  = new Contact(ip: "127.0.0.1", port: "8080", name : "jack", isValid: true)
         contact.save()
+        sleep(500l)
 
-        assert hasCalledWS == true
+        assert hasFiredDisplayEvent == true
     }
 }
